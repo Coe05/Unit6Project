2cd599a2697e7da846d0c3b962926386b632ae3c
Coe05 <120431229+Coe05@users.noreply.github.com>
2023-04-04 07:50:45 -0500

    d

---
+++ Term.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Term here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Term
+{
+   private int coefficent, exponent; 
+   public Term(int a, int b)
+   {
+       coefficent = a; 
+       exponent = b; 
+   }
+}
