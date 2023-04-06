812e1545dbd53db4cc280d6bdaa0fd3837fc8ae2
Coe05 <elicoe05@icloud.com>
2023-04-05 22:23:55 -0500

    f

--- Polynomials.java
+++ Polynomials.java
@@ -10,12 +10,12 @@ public class Polynomials
     public static void main (String [] args)
     {
         Scanner reader = new Scanner(System.in);
-        
+
         ArrayList<Term> Mon1 = new ArrayList<Term>();
         ArrayList<Term> Mon2 = new ArrayList<Term>();
         int x = 0; 
         int y = 0; 
-        
+
         System.out.println("Enter 0 0 to stop."); 
         do{
             x = reader.nextInt(); 
@@ -30,38 +30,34 @@ public class Polynomials
             Term z = new Term(x,y);
             Mon2.add(z);
         }while(x != 0 & y != 0);
-        
-        
-        
+
         System.out.println(Mon1);
         for(int q = 0; q<Mon1.size(); q++)
         {
             System.out.print(Mon1.get(q) + " + "); 
         }
-        System.out.println(); 
-        
-        
-        
+        sort(Mon1);
+
+    }
+    public static void sum(ArrayList<Term> list)
+    {
         
     }
     public static void sort(ArrayList<Term> list)
     {
-        for(int x= 0; x<list.size(); x++)
+        for(int x = 0; x<list.size(); x++)
         {
             boolean flag = true;
             for(int y = 0; y<list.size()-1-x; y++)
             {
-                if(list.get(y)>list.get(y+1))
+                if(list.getExponent(y) > list.getExponent(y+1))
                 {
                     int temp = list.remove(y); 
                     list.add(y+1, temp); 
                     flag = false;
                 }
             }
-            if(flag)
-            {
-                break;
-            }
         }
     }
+
 }
