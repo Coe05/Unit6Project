2e46535151a05eb7db76f2498603e0ad7f87a822
Coe05 <120431229+Coe05@users.noreply.github.com>
2023-05-15 16:37:44 -0500

    d

---
+++ dasa.java
@@ -0,0 +1,42 @@
+
+/**
+ * Write a description of class dasa here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class dasa
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class dasa
+     */
+    public dasa()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+    public static void sasha ()
+    {
+        
+    }
+    public void sasa()
+    {
+        
+    }
+    
+}
